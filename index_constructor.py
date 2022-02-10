241ffe9e4b80890ccc7551a6c01f31fdad01e648
quangnluci <quangnl@uci.edu>
2022-02-10 13:59:59 -0800

    handled title, heading, bold before normal texts. each entry in index is a list of [dict, type, tfidf]. default type to highest value.

--- index_constructor.py
+++ index_constructor.py
@@ -1,4 +1,9 @@
-import nltk
+from nltk.tokenize import WhitespaceTokenizer
+from nltk.stem import WordNetLemmatizer
+import nltk.corpus
+from string import punctuation
+import json
+from bs4 import BeautifulSoup
 
 # remove stop words
 # lemmatize remaining tokens
@@ -17,11 +22,154 @@ def get_tf_idf():
     pass
 
 
-class InvertedIndex():
+class InvertedIndex:
     def __init__(self):
-        self.stopwords = []
         self.document_count = 0
-        self.index = {} # only for inverted index
+        self.index = {}     # only for inverted index
         self.title_index = {}
         self.heading_index = {}
         self.bold_index = {}
+
+        self.stopwords = []
+        with open('stopwords.txt') as file:
+            for line in file:
+                self.stopwords.append(line.rstrip())
+
+    def read_corpus(self, corpus_path):
+        nltk_words = set(nltk.corpus.words.words())
+        # get the bookkeeping file
+        with open(corpus_path + '\\bookkeeping.json') as f:
+            bookkeeping = json.load(f)
+
+        # for each entry in bookkeeping, navigate to the folder and then file, and read it with BS
+        # first number is folder
+        # second number is file
+        for key in bookkeeping:
+            folder_file = key.split('/')
+            if folder_file[0] != '0':   # hardcoded limit, remove when ready for full corpus
+                break
+            with open(corpus_path + '\\' + folder_file[0] + '\\' + folder_file[1], 'rb') as f:
+                page = BeautifulSoup(f, 'html.parser')
+
+            tokenizer = WhitespaceTokenizer()
+            lemmatizer = WordNetLemmatizer()
+
+            # Handle titles, headings, and bold texts first
+            title = page.find('title')
+            heading_1 = page.find('h1')
+            heading_2 = page.find('h2')
+            heading_3 = page.find('h3')
+            bold = page.find('b')
+            if title and not title.string == "":
+                try:
+                    text_tokens = tokenizer.tokenize(title.string)
+                    text_tokens = [w for w in text_tokens if w.lower() in nltk_words]
+                    text_tokens_len = len(text_tokens)
+                    for i in range(0, text_tokens_len):
+                        text_tokens[i] = lemmatizer.lemmatize(str.lower(text_tokens[i].strip(punctuation)))
+                    # add to inverted index
+                    # TODO: UPDATE FREQUENCY PROPERLY (is this correct?)
+                    for word in text_tokens:
+                        if word not in self.stopwords:
+                            if word in self.index and key in self.index[word][0]:  # increment frequency in this document
+                                self.index[word][0][key] += 1
+                            else:  # create new entry for this document
+                                self.index[word] = [{key: 1}, 4, 0]  # List order: [{docID: freq}, type, tf-idf]
+                except TypeError:
+                    pass
+
+            if heading_1 and not heading_1.string == "":
+                try:
+                    text_tokens = tokenizer.tokenize(heading_1.string)
+                    text_tokens = [w for w in text_tokens if w.lower() in nltk_words]
+                    text_tokens_len = len(text_tokens)
+                    for i in range(0, text_tokens_len):
+                        text_tokens[i] = lemmatizer.lemmatize(str.lower(text_tokens[i].strip(punctuation)))
+                    # add to inverted index
+                    # TODO: UPDATE FREQUENCY PROPERLY (is this correct?)
+                    for word in text_tokens:
+                        if word not in self.stopwords:
+                            if word in self.index and key in self.index[word][0]:  # increment frequency in this document
+                                self.index[word][0][key] += 1
+                            else:  # create new entry for this document
+                                self.index[word] = [{key: 1}, 3, 0]  # List order: [{docID: freq}, type, tf-idf]
+                except TypeError:
+                    pass
+
+            if heading_2 and not heading_2.string == "":
+                try:
+                    text_tokens = tokenizer.tokenize(heading_2.string)
+                    text_tokens = [w for w in text_tokens if w.lower() in nltk_words]
+                    text_tokens_len = len(text_tokens)
+                    for i in range(0, text_tokens_len):
+                        text_tokens[i] = lemmatizer.lemmatize(str.lower(text_tokens[i].strip(punctuation)))
+                    # add to inverted index
+                    # TODO: UPDATE FREQUENCY PROPERLY (is this correct?)
+                    for word in text_tokens:
+                        if word not in self.stopwords:
+                            if word in self.index and key in self.index[word][0]:  # increment frequency in this document
+                                self.index[word][0][key] += 1
+                            else:  # create new entry for this document
+                                self.index[word] = [{key: 1}, 3, 0]  # List order: [{docID: freq}, type, tf-idf]
+                except TypeError:
+                    pass
+
+            if heading_3 and not heading_3.string == "":
+                try:
+                    text_tokens = tokenizer.tokenize(heading_3.string)
+                    text_tokens = [w for w in text_tokens if w.lower() in nltk_words]
+                    text_tokens_len = len(text_tokens)
+                    for i in range(0, text_tokens_len):
+                        text_tokens[i] = lemmatizer.lemmatize(str.lower(text_tokens[i].strip(punctuation)))
+                    # add to inverted index
+                    # TODO: UPDATE FREQUENCY PROPERLY (is this correct?)
+                    for word in text_tokens:
+                        if word not in self.stopwords:
+                            if word in self.index and key in self.index[word][0]:  # increment frequency in this document
+                                self.index[word][0][key] += 1
+                            else:  # create new entry for this document
+                                self.index[word] = [{key: 1}, 3, 0]  # List order: [{docID: freq}, type, tf-idf]
+                except TypeError:
+                    pass
+
+            if bold and not bold.string == "":
+                try:
+                    text_tokens = tokenizer.tokenize(bold.string)
+                    text_tokens = [w for w in text_tokens if w.lower() in nltk_words]
+                    text_tokens_len = len(text_tokens)
+                    for i in range(0, text_tokens_len):
+                        text_tokens[i] = lemmatizer.lemmatize(str.lower(text_tokens[i].strip(punctuation)))
+                    # add to inverted index
+                    # TODO: UPDATE FREQUENCY PROPERLY (is this correct?)
+                    for word in text_tokens:
+                        if word not in self.stopwords:
+                            if word in self.index and key in self.index[word][0]:  # increment frequency in this document
+                                self.index[word][0][key] += 1
+                            else:  # create new entry for this document
+                                self.index[word] = [{key: 1}, 2, 0]  # List order: [{docID: freq}, type, tf-idf]
+                except TypeError:
+                    pass
+
+            # tokenize
+            # Whitespace tokenizer - keep contractions because they're in stopwords
+            text_tokens = tokenizer.tokenize(page.get_text())
+            # delete non-english words
+            text_tokens = [w for w in text_tokens if w.lower() in nltk_words]
+
+            # lemmatize - must strip leading and trailing punctuation because whitespace
+            # tokenizer leaves them in
+            text_tokens_len = len(text_tokens)
+            for i in range(0, text_tokens_len):
+                text_tokens[i] = lemmatizer.lemmatize(str.lower(text_tokens[i].strip(punctuation)))
+
+            # add to inverted index
+            # TODO: UPDATE FREQUENCY PROPERLY (is this correct?)
+            for word in text_tokens:
+                if word not in self.stopwords:
+                    if word in self.index and key in self.index[word][0]:  # increment frequency in this document
+                        self.index[word][0][key] += 1
+                        pass
+                    else:  # create new entry for this document
+                        self.index[word] = [{key: 1}, 1, 0]  # List order: [{docID: freq}, type, tf-idf]
+
+        print(self.index)
--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-import json
+from operator import inv
 import sys
 from bs4 import BeautifulSoup
-import index_constructor
+from index_constructor import InvertedIndex
 
 if __name__ == "__main__":
 
-   # get directory of the webpages file
-    webpages_folder = sys.argv[1]
-
-    # get the bookkeeping file
-    with open(webpages_folder + '\\bookkeeping.json') as f:
-        bookkeeping = json.load(f)
-
-    # for each entry in bookkeeping, navigate to the folder and then file, and read it with BS
-    # first number is folder
-    # second number is file
-    for key in bookkeeping:
-        print(key)
-        folder_file = key.split('/')
-        if folder_file[0] != '0': # hardcoded limit, remove when ready for full corpus
-            break
-        print(bookkeeping[key])
-        with open(webpages_folder + '\\' + folder_file[0] + '\\' + folder_file[1], 'rb') as f:
-            page = BeautifulSoup(f)
-            title = page.find('title')
-            heading = page.find('h1')
-            bold = page.find('b')
-        print(bold)
+    # get directory of the webpages file, feed to constructor
+    inv_idx = InvertedIndex()
+    inv_idx.read_corpus(sys.argv[1])
 
 # remove stop words
 # lemmatize remaining tokens
