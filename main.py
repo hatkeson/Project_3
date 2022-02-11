c8e1b726fb2afa0b768901c69a317d2b0f26bfd7
quangnluci <quangnl@uci.edu>
2022-02-11 02:06:38 -0800

    loaded the json file. Still unsure on how to read/restructure it

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from operator import inv
 import sys
+import json
 from bs4 import BeautifulSoup
 from index_constructor import InvertedIndex
 
 if __name__ == "__main__":
 
     # get directory of the webpages file, feed to constructor
-    inv_idx = InvertedIndex()
-    inv_idx.read_corpus(sys.argv[1])
-    inv_idx.calculate_tf_idf()
-    inv_idx.write_to_json()
+    # inv_idx = InvertedIndex()
+    # inv_idx.read_corpus(sys.argv[1])
+    # inv_idx.calculate_tf_idf()
+    # inv_idx.write_to_json()
 
+    # Get index dict from json file
+    with open('index_text_file.json') as file:
+        index = json.load(file)
+        #print(index[0])
+
+
+    # # Get user input
+    # while True:
+    #     query = input("Enter query: ")
+    #     print(query)
+    #
+    #     # We have index, now we can look up the query/word on the index
+    #     print(index[query])
 
 # remove stop words
 # lemmatize remaining tokens
