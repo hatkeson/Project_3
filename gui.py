ed664d29ffd3cc549561e8d07d8e7e88bc747e90
hatkeson <hatkeson@socal.rr.com>
2022-02-23 21:32:11 -0800

    results list diplays results

--- gui.py
+++ gui.py
@@ -8,26 +8,40 @@ def search_return_key(event):
     search()
 
 def search():
+    for item in results_list.get_children():
+            results_list.delete(item)
     q = query_var.get()
     if q:
-        print(q)
+        result_count = 0
+        for result in index_dict[q]:
+            if result in bookkeeping and result_count < 20:
+                print(bookkeeping[result])
+                results_list.insert('', 'end', text=bookkeeping[result])
+                result_count += 1
+        print(str(result_count) + ' results.')
 
+with open('index_text_file.json') as file:
+    index = json.load(file)
+    index_dict = json.loads(index)
 
-root = Tk()
-root.title("Search Engine")
+with open(r'.\WEBPAGES_RAW\bookkeeping.json') as b:
+    bookkeeping = json.load(b)
 
-frm = ttk.Frame(root, padding=10)
-frm.grid()
+root = Tk() # creates window
+root.title("Search Engine") # gives window a title
 
-query_var = tk.StringVar()
-entry = ttk.Entry(frm, textvariable=query_var)
-entry.grid(column=0, row=0)
-entry.bind("<Return>", search_return_key)
+frm = ttk.Frame(root, padding=10) # creates frame inside window
+frm.grid() # we want elements laid out in a grid (other option is "pack")
 
-search_button = ttk.Button(frm, text="Search", command=search)
-search_button.grid(column=1, row=0)
+query_var = tk.StringVar() # this stores user input
+entry = ttk.Entry(frm, textvariable=query_var) # text box where user enters query
+entry.grid(column=0, row=0) # we'll place it in cell (0,0) in the grid
+entry.bind("<Return>", search_return_key) # user triggers search function by pressing enter
 
-results_list = ttk.Treeview(frm)
-results_list.grid(column=0, row=2)
+search_button = ttk.Button(frm, text="Search", command=search) # command is pointer to function
+search_button.grid(column=1, row=0) # put it in cell (0,1)
 
-root.mainloop()
+results_list = ttk.Treeview(frm, height=20) # where results will be displayed
+results_list.grid(column=0, row=2) # put it in cell (2,0)
+
+root.mainloop() # run the window
